2cf364574439b836f8cf26eb9cb7006464994db1
milisic <matija.milisic@gmail.com>
2017-01-10 00:43:54 +0100

    Added compiled code for google testing on ubuntu.

--- code/bin/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ -isystem ../include -pthread ../test/ExampleTest.cpp libgtest.a -o test && ./test
---
+++ code/bin/ubuntu/run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+g++ -isystem ../../include -pthread ../../test/ExampleTest.cpp libgtest.a -o test && ./test
