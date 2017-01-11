5b350a5634e05f3fabb9bf621053d8ae03bfdba0
Mateo Simonovic <mateosimonovic@gmail.com>
2017-01-11 19:07:28 +0100

    Fixed Windows test script. Added output files to gitignore

--- code/bin/ubuntu/run_tests.sh
+++ code/bin/ubuntu/run_tests.sh
@@ -5,5 +5,5 @@ do
 	file_name=$(basename $test_file);
 	test_name="${file_name%.*}"
 	echo "Running test file:" $file_name;
-	g++ -isystem ../../include -pthread $test_file ../../src/RRR.cpp libgtest.a -o $test_name && ./$test_name
+	g++ -isystem ../../include -pthread $test_file ../../src/RRR.cpp libgtest.a -o $test_name.out && ./$test_name.out
 done
