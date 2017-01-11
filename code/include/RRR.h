a179451a26a435acd12da45593722a92224e75d6
milisic <matija.milisic@gmail.com>
2017-01-11 19:02:38 +0100

    RRR Test skeleton.

--- code/include/RRR.h
+++ code/include/RRR.h
@@ -34,3 +34,4 @@ class RRR {
 
 };
 #endif
+
--- code/test/RRRTest.cpp
+++ code/test/RRRTest.cpp
@@ -1,5 +1,9 @@
+#include <vector>
+
 #include "../include/gtest/gtest.h"
 #include "../include/RRR.h"
+
+using namespace std;
 
 TEST (RRRTest, Rank0) {
     
@@ -18,6 +22,9 @@ TEST (RRRTest, Access) {
 }
 
 int main(int argc, char **argv) {
+  vector<bool> v(5, false);
+  RRR rrr(v, 2, 5);
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
