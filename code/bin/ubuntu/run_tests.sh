f5b33a08c270f303dc02f1b732fcd70e80581ecd
Mateo Simonovic <mateosimonovic@gmail.com>
2017-01-11 17:11:10 +0100

    Test scripts compile & run all tests

--- code/bin/ubuntu/run_tests.sh
+++ code/bin/ubuntu/run_tests.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-g++ -isystem ../../include -pthread ../../test/ExampleTest.cpp libgtest.a -o test && ./test
+for test_file in $(find ../../test/ -name "*.cpp")
+do
+	file_name=$(basename $test_file);
+	test_name="${file_name%.*}"
+	echo "Running test file:" $file_name;
+	g++ -isystem ../../include -pthread $test_file libgtest.a -o $test_name && ./$test_name
+done
